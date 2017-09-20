14e73a6e339a0aa97909cddae654dd9cff30d9a8
YauheniIhnatsyeu <tyz1@list.ru>
2017-09-20 21:31:21 +0300

    Azz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function zeros(expression) {
-  // your solution
+﻿module.exports = function zeros(expression) {
+    expression = expression.split('*');
+    var fiveNumber = 0;
+    var twoNumber = 0;
+
+    var skipFlag = false;
+    var j = 0;
+    do {
+
+        for (let i = parseInt(expression[j]); i > 0; i -= checkSkipFlag(expression[j]) ? 2 : 1) {
+            if (i % 10 == 2) ++twoNumber;
+            if (i % 5 == 0) ++fiveNumber;
+            if (i % 25 == 0) ++fiveNumber;
+        }
+
+    } while (j++ <= expression.length);
+    return twoNumber ? fiveNumber : twoNumber;
+}
+
+function checkSkipFlag(val = "") {
+    return val.includes("!!");
 }
